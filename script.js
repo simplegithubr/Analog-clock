1908090e5b6d286a0b49b392ec3bfd87b5ae315b
sagar sheikh <160107305+simplegithubr@users.noreply.github.com>
2024-11-06 14:59:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let hr = document.getElementById("hour");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+function displayTime() {
+    let date = new Date();
+    // Getting hours, minutes, and seconds from date
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    // Rotation for each hand
+    let hRotation = 30 * hh + mm / 2;    // Hour hand rotation
+    let mRotation = 6 * mm;              // Minute hand rotation
+    let sRotation = 6 * ss;              // Second hand rotation
+
+    // Applying the rotations to the elements
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+}
+
+setInterval(displayTime, 1000);
